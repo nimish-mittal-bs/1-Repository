858232cd0b8ee1a99aa58466a16e04641cb52073
nimish-mittal-bs <nimish.mittal@bigsteptech.com>
2021-04-26 18:15:52 +0530

    first commit

---
+++ moon/add.js
@@ -0,0 +1,7 @@
+let btnadd=document.querySelector('#add');
+let btnremove=document.querySelector('#remove');
+let output=document.querySelector('h1');
+
+btnadd.addEventListener('click',()=>{
+    document.addEventListener
+})
